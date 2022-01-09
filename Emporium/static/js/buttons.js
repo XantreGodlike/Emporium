cea25756c1c4a8be47bce8f04e45df755f0db795
Valery Smirnov <valerik007.vs@gmail.com>
2022-01-09 13:06:46 +0300

    Made buttons

---
+++ Emporium/static/js/buttons.js
@@ -0,0 +1,9 @@
+// let buttons = document.getElementsByClassName("button");
+
+// for (let button of buttons){
+//     let computed = getComputedStyle(button);
+//     let height = parseInt(computed.height);
+//     let width = parseInt(computed.width);
+
+//     button.style.fontSize = `${(40/85*height/width*100)}%`;
+// }
